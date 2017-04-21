68715fb15394a0dba83cb60914f6ce3271fae627
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-04-21 18:39:12 +0200

    init

--- test/test_app.js
+++ test/test_app.js
@@ -6,9 +6,6 @@ var helpers = require('yeoman-test');
 
 beforeEach(function () {  
   return helpers.run(path.join(__dirname, '../app'))
-    .withOptions({ foo: 'bar' })
-    .withArguments(['name-x'])
-    .withPrompts({ coffee: false });
 })
 
 describe('java-bones:app', function () {
@@ -20,9 +17,8 @@ describe('java-bones:app', function () {
       'VERSIONS.md', 
       '.gitignore', '.gitattributes', '.travis.yml',       
       'build.sh', 'emptyproject4j', 
-      'checkstyle.xml', 'ruleset.xml', 
-      
-      'pom.xml',       
+      'checkstyle.xml', 'ruleset.xml',       
+      'pom.xml',
       'src/main/java/net/groupID/emptyproject4j/Main.java',
       'src/test/java/net/groupID/tests/emptyproject4j/TestSuite.java']);
   });
